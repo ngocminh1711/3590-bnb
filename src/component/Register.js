71ceecc1e3d0d62ce696fe235665dc6f90ae3acc
unknown <dodfood>
2022-11-01 15:02:32 +0700

    update register

---
+++ src/component/Register.js
@@ -0,0 +1,126 @@
+import {useState} from 'react'
+import axios from 'axios'
+// import { Navigate } from 'react-router-dom'
+function Register() {
+  const [message,setMessage]= useState("")
+  const [form,setForm]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+  console.log(form)
+//   const navigate = Navigate()
+  const [registersuscess,setRegisterSuscess]= useState(false)
+  const handleChange = (e)=>{
+    setForm({...form,[e.target.name]:e.target.value})
+  }
+  const handleRegister = async()=>{
+    console.log(1)
+    const res = await axios.post ("http://localhost:8000/api/auth/register",form)
+    console.log(res)
+      if(res.data.success === true){
+        setRegisterSuscess(true)
+      }
+  }
+  return (
+    <>
+      {" "}
+      <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200" />
+        </div>
+      </div>
+      <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="mt-5 md:col-span-2 md:mt-0">
+            <form action="#" method="POST">
+              <div className="overflow-hidden shadow sm:rounded-md">
+                <div className="bg-white px-4 py-5 sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="last-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        name="username"
+                        id="last-name"
+                        onChange={handleChange}
+                        autoComplete="family-name"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-4">
+                      <label
+                        htmlFor="email-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="text"
+                        name="email"
+                        id="email-address"
+                        onChange = {handleChange}
+                        autoComplete="email"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="text"
+                        name="password"
+                        id="street-address"
+                        onChange = {handleChange}
+                        autoComplete="street-address"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        name="password"
+                        onChange={handleChange}
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Retype Password
+                      </label>
+                      <input
+                        type="text"
+                        name="city"
+                        id="city"
+                        autoComplete="address-level2"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    onSubmit ={handleRegister}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Register;

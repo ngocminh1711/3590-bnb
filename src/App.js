c233a928446c2f7c4efd675808c23e2680bec7d1
huydothe <you@example.com>
2022-11-01 16:51:31 +0700

    search template

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
+import Home from "./components/home/Index.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   );
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,44 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const navigate = useNavigate();
+  const [errMessage, setErrMessage] = useState('');
 
+  const PORT = process.env.PORT || 8000;
 
-    
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let data = {
+      username: form.username,
+      password: form.password,
+    };
+    axios
+      .post(`http://localhost:8000/login`, data)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          localStorage.setItem("token", JSON.stringify(res.data.token));
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        setErrMessage(err.response.data.message)
+      });
+  };
+
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
           <img
             className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src="https://links.papareact.com/qd3"
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign in to your account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or
-            <a
-              href="#"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              start your 14-day free trial
-            </a>
-          </p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        {errMessage ?
+          <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+          <span class="font-medium">Error</span> {errMessage}
+        </div> 
+: null}
+        <form className="mt-8 space-y-6" onSubmit={(e) => handleSubmit(e)}>
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
-              <label htmlFor="email-address" className="sr-only">
+              <label htmlFor="username" className="sr-only">
                 Email address
               </label>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="username"
+                name="username"
+                type="text"
+                value={form.username}
+                onChange={(e) => handleChange(e)}
                 required
                 className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Username"
               />
             </div>
             <div>
@@ -49,7 +82,8 @@ function Login() {
                 id="password"
                 name="password"
                 type="password"
-                autoComplete="current-password"
+                value={form.password}
+                onChange={(e) => handleChange(e)}
                 required
                 className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Password"

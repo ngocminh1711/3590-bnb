87ce9c74a06821052969ea56348bb3359899c2d6
huydothe <you@example.com>
2022-11-02 11:38:19 +0700

    logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "./App.css";
-import Header from "./components/header/Header";
+// import Header from "./components/header/Header";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Home from "./components/home/Index.js";
@@ -10,7 +10,7 @@ import CreateHouseForRent from "./components/CreateHouseForRent/CreateHouseForRe
 function App() {
     return (
         <>
-            <Header />
+            
             <Routes>
                 <Route path='/login' element={<Login />} />
                 <Route path='/' element={<Home />} />

d1bfc763b8abf85ca80409ad6a7940383194300c
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-11-03 14:51:05 +0700

    show detail house for rent done

--- src/components/DetailHouseForRent/DetailHouseForRent.js
+++ src/components/DetailHouseForRent/DetailHouseForRent.js
@@ -101,7 +101,7 @@ function DetailHouseForRent() {
                         </svg>
                         <div className="grid grid-rows-2">
                             <h1 className="text-black-300 text-1.5xl title-font font-medium mb-1 float-left">Designed by</h1>
-                            <p className="text-gray-500 text-1.5xl">abc</p>
+                            <p className="text-gray-500 text-1.5xl">Ngo Minh Ngoc</p>
                         </div>
                     </div>
                     <div>
@@ -120,20 +120,31 @@ function DetailHouseForRent() {
                                 <div className="m-auto">
                                         <div className="mt-5 rounded-lg shadow">
                                             <h1 className="text-gray-900 text-2xl title-font font-medium mb-1 px-7 py-5 pb-5 inline">$ {houseForRent.roomRates} night</h1>
-                                            <div className="px-5 pb-5">
-                                                <div className=" grid grid cols-2 rounded-full border-x-10 border-y-10 border-red-100">
+                                            <div className="px-5 pb-5 pt-5">
+                                                <div className=" rounded-full border grid grid-cols-2 grid-flow-row ">
                                                     <input type="date"/>
                                                     <input type="date"/>
-                                                </div>
-                                                <div className="flex flex-row-reverse p-3">
-                                                    <div className="flex-initial pl-3">
-                                                        <button type="submit"
-                                                                className="bg-rose-500 hover:bg-rose-400 text-white font-bold py-2 px-4 rounded-full">
-                                                            <span>Reserve</span>
-                                                        </button>
+                                                    <div className="col-span-2">
+                                                        <select className="w-full">
+                                                            <option>1</option>
+                                                            <option>1</option>
+                                                            <option>1</option>
+                                                        </select>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div className="px-5 pt-0">
+                                                <button type="submit"
+                                                        className="bg-rose-500 hover:bg-rose-400 text-white font-bold py-2 px-4 rounded-lg border w-full">
+                                                    <span>Reserve</span>
+                                                </button>
+
+                                            </div>
+                                            <div className="px-5 pt-0">
+                                                <h1 className="text-gray-900 text-1.75xl title-font font-medium mb-1 px-7 py-5 pb-5 inline">Total before taxes  </h1>
+                                                <h1 className="text-gray-900 text-1.75xl title-font font-medium mb-1 px-7 py-5 pb-5 inline">$  </h1>
+                                            </div>
+
                                     </div>
 
                                 </div>
@@ -142,12 +153,52 @@ function DetailHouseForRent() {
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-4 py-10 px-24 pt-0">
+            <div className="grid grid-cols-2 gap-4 py-10 px-24 pt-0 pb-0">
                 <h1 className=" text-black-300 text-1xl title-font font-normal mb-1">
                         {houseForRent.description}
                 </h1>
             </div>
-            <div className="grid grid-cols-2 gap-4 py-10 px-24">
+            <div className="grid grid-cols-2  gap-4 py-10 px-24 pr-0">
+                <div className="grid grid-cols-2 grid-rows-3 gap-4 py-10 px-24 pl-0 pt-0">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p className="inline pl-2">City skyline view</p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+                        </svg>
+                        <p className="inline pl-2">Wifi </p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
+                        </svg>
+                        <p className="inline pl-2">TV </p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                        </svg>
+                        <p className="inline pl-2">Security cameras on property </p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                        </svg>
+                        <p className="inline pl-2">Free parking on premises </p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                        <p className="inline pl-2">Kitchen </p>
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-4 py-10 px-24 pt-0">
                 <div>
                     <h1 className="text-gray-450 text-2xl title-font font-medium mb-1">Where you'll sleep</h1>
                     <div className=" grid grid-cols-3 gap-4">

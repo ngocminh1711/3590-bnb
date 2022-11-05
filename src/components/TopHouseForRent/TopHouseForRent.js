dcf8a3e21b42e901867157c32eb394d4688672f0
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-11-05 21:27:48 +0700

    test pull huy

--- src/components/TopHouseForRent/TopHouseForRent.js
+++ src/components/TopHouseForRent/TopHouseForRent.js
@@ -28,10 +28,11 @@ function TopHouseForRent() {
 
     }, [])
     return (
-        <div className="pt-0">
+        <div>
             <div className="bg-white">
-                <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 pt-0">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Top 4 houses with the most
+                <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900 pt-0">Top 4 houses with the most
                         tenants</h2>
 
                     <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
